6927d80e661cb76c34652415e2f9ec1388e843db
mathuria <mathuria@adobe.com>
2024-07-09 21:04:00 +0530

    Initial hierarchy checkin

--- unitylibs/scripts/utils.js
+++ unitylibs/scripts/utils.js
@@ -40,23 +40,3 @@ export async function useMiloSample() {
 
 export const unityConfig = {
 }
-
-export const [setUnityLibs, getUnityLibs] = (() => {
-  let libs;
-  return [
-    (prodLibs, project = 'unity') => {
-      const { hostname, origin} = window.location;
-      if (!hostname.includes('hlx.page')
-        && !hostname.includes('hlx.live')
-        && !hostname.includes('localhost')) {
-        libs = prodLibs;
-        return libs;
-      }
-      if (project === 'unity') { libs = `${origin}/unitylibs`; return libs; }
-      const branch = new URLSearchParams(window.location.search).get('unitylibs') || 'main';
-      if (branch.indexOf('--') > -1) { libs = `https://${branch}.hlx.live/unitylibs`; return libs; }
-      libs = `https://${branch}--unity--adobecom.hlx.live/unitylibs`;
-      return libs;
-    }, () => libs,
-  ];
-})();
