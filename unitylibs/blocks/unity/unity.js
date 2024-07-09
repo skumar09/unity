9135a101031c543847159e9e4ba0791f0d2ad4a0
Aishwarya Mathuria <mathuria@adobe.com>
2024-07-09 20:15:50 +0530

    Update unity.js

--- unitylibs/blocks/unity/unity.js
+++ unitylibs/blocks/unity/unity.js
@@ -12,9 +12,9 @@ export const [setUnityLibs, getUnityLibs] = (() => {
         libs = prodLibs;
         return libs;
       }
+      if (project === 'unity') { libs = `${origin}/unitylibs`; return libs; }
       const branch = new URLSearchParams(window.location.search).get('unitylibs') || 'main';
       if (branch.indexOf('--') > -1) { libs = `https://${branch}.hlx.live/unitylibs`; return libs; }
-      if (hostname.includes('localhost') && (project === 'unity')) { libs = `${origin}/unitylibs`; return libs; }
       libs = `https://${branch}--unity--adobecom.hlx.live/unitylibs`;
       return libs;
     }, () => libs,
