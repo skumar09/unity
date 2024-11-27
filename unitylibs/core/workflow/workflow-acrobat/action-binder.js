61e91a06f34475862ba62e70b94dc24ba5864ddf
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-11-26 16:12:06 -0800

    MWPW-162758-Add cookie check for upload events (#166)
    - Add cookie check for upload events before redirect
    
    Resolves: [MWPW-162758](https://jira.corp.adobe.com/browse/MWPW-162758)
    
    Test URLs:
    
    Before:
    https://stage--cc--adobecom.hlx.page/acrobat/online/sign-pdf?unitylibs=stage
    After:
    https://stage--cc--adobecom.hlx.page/acrobat/online/sign-pdf?unitylibs=analytics-cookie
    
    Note: As the cookie is set only for adobe.com domain test by accessing
    above url on stage.adobe.com and overriding the action-binder.js with
    the code form this feature branch

--- unitylibs/core/workflow/workflow-acrobat/action-binder.js
+++ unitylibs/core/workflow/workflow-acrobat/action-binder.js
@@ -85,7 +85,6 @@ export default class ActionBinder {
           break;
         case value.actionType === 'continueInApp':
           this.LOADER_LIMIT = 100;
-          this.updateProgressBar(this.splashScreenEl, 100);
           await this.continueInApp();
           break;
         case value.actionType === 'interrupt':
@@ -157,18 +156,19 @@ export default class ActionBinder {
 
   async dispatchErrorToast(code, status, info = null, showError = true) {
     if (showError) {
-      const message = code in this.workflowCfg.errors
+      const errorMessage = code in this.workflowCfg.errors
         ? this.workflowCfg.errors[code]
         : await (async () => {
           const getError = (await import('../../../scripts/errors.js')).default;
           return getError(this.workflowCfg.enabledFeatures[0], code);
         })();
+      const message = code.includes('cookie_not_set') ? '' : errorMessage || 'Unable to process the request';
       this.block.dispatchEvent(new CustomEvent(
         unityConfig.errorToastEvent,
         {
           detail: {
-            code, 
-            message: `${message || 'Unable to process the request'}`, 
+            code,
+            message: `${message}`,
             status,
             info,
             accountType: this.getAccountType(),
@@ -225,6 +225,27 @@ export default class ActionBinder {
     await Promise.all(this.promiseStack);
   }
 
+  checkCookie = () => {
+    const cookies = document.cookie.split(';').map((item) => item.trim());
+    const targets = [/^UTS_Uploading=/, /^UTS_Uploaded=/];
+    return targets.every((regex) => cookies.some((item) => regex.test(item)));
+  };
+
+  waitForCookie = (timeout) => {
+    return new Promise((resolve) => {
+      const interval = 100;
+      let elapsed = 0;
+      const intervalId = setInterval(() => {
+        if (this.checkCookie() || elapsed >= timeout) {
+          clearInterval(intervalId);
+          resolve();
+        }
+        elapsed += interval;
+      }, interval);
+    });
+  };
+
+
   async continueInApp() {
     if (!this.operations.length) return;
     const { assetId, filename, filesize, filetype } = this.operations[this.operations.length - 1];
@@ -251,10 +272,16 @@ export default class ActionBinder {
       ),
     );
     await Promise.all(this.promiseStack)
-      .then((resArr) => {
+      .then(async (resArr) => {
         const response = resArr[resArr.length - 1];
         if (!response?.url) throw new Error('Error connecting to App');
         this.block.dispatchEvent(new CustomEvent(unityConfig.trackAnalyticsEvent, { detail: { event: 'redirect to product' } }));
+        await this.waitForCookie(2000);
+        this.updateProgressBar(this.splashScreenEl, 100);
+        if (!this.checkCookie()) {
+          await this.dispatchErrorToast('verb_cookie_not_set', 200, "Not all cookies found, redirecting anyway", true);
+          await new Promise(r => setTimeout(r, 500));
+        }
         window.location.href = response.url;
       })
       .catch(async (e) => {
