fe6600f3dfa62a100b7b99baa442e954500ac1b7
Aishwarya Mathuria <mathuria@adobe.com>
2025-02-27 14:16:41 +0530

    MWPW-165557 | Unit tests for unity (#280)
    Unit tests for unity
    
    Resolves: [MWPW-165557](https://jira.corp.adobe.com/browse/MWPW-165557)
    
    **Test URLs:**
    - Before: https://main--unity--adobecom.aem.page/?martech=off
    - After: https://unittest--unity--adobecom.aem.page/?martech=off

--- test/scripts/utils.test.js
+++ test/scripts/utils.test.js
@@ -42,4 +42,9 @@ describe('Libs', () => {
     const libs = setLibs('/libs', location);
     expect(libs).to.equal('https://awesome--milo--forkedowner.aem.live/libs');
   });
+
+  it('Should return libs', () => {
+    const libs = getLibs();
+    expect(libs).to.equal('https://awesome--milo--forkedowner.aem.live/libs');
+  });
 });
