0c4d94f3fae79ea087d0a6d659540b53ea1528be
Sharmee Biswas <sharmeeadobe@gmail.com>
2025-03-27 07:25:51 +0530

    UTs for common header and token function (#312)
    UTs to improve coverage for common headers and token function

--- test/scripts/utils.test.js
+++ test/scripts/utils.test.js
@@ -1,5 +1,6 @@
 import { expect } from '@esm-bundle/chai';
-import { setLibs, getLibs } from '../../unitylibs/scripts/utils.js';
+import sinon from 'sinon';
+import { setLibs, getLibs, getHeaders } from '../../unitylibs/scripts/utils.js';
 
 describe('Libs', () => {
   it('Default Libs', () => {
@@ -46,5 +47,56 @@ describe('Libs', () => {
   it('Should return libs', () => {
     const libs = getLibs();
     expect(libs).to.equal('https://awesome--milo--forkedowner.aem.live/libs');
+  });
+});
+
+describe('Headers and Token', () => {
+  let adobeIMSStub;
+
+  beforeEach(() => {
+    window.adobeIMS = { getAccessToken: () => {}, refreshToken: () => {} };
+    adobeIMSStub = sinon.stub(window.adobeIMS, 'getAccessToken');
+  });
+
+  afterEach(() => {
+    adobeIMSStub.restore();
+  });
+
+  it('Should return headers with guest access token', async () => {
+    const token = { token: 'guest-token', expire: Date.now() + (10 * 60 * 1000) };
+    adobeIMSStub.returns(token);
+
+    const headers = await getHeaders('test-api-key');
+    expect(headers).to.deep.equal({
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer guest-token',
+      'x-api-key': 'test-api-key',
+    });
+  });
+
+  it('Should return headers with refreshed token if guest access token is expired', async () => {
+    const token = { token: 'guest-token', expire: Date.now() - (10 * 60 * 1000) };
+    adobeIMSStub.returns(token);
+    sinon.stub(window.adobeIMS, 'refreshToken').resolves({ tokenInfo: { token: 'refreshed-token' } });
+
+    const headers = await getHeaders('test-api-key');
+    expect(headers).to.deep.equal({
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer refreshed-token',
+      'x-api-key': 'test-api-key',
+    });
+  });
+
+  it('Should return headers with empty string if refresh token fails', async () => {
+    const token = { token: 'guest-token', expire: Date.now() - (10 * 60 * 1000) };
+    adobeIMSStub.returns(token);
+    sinon.stub(window.adobeIMS, 'refreshToken').rejects(new Error('Refresh token failed'));
+
+    const headers = await getHeaders('test-api-key');
+    expect(headers).to.deep.equal({
+      'Content-Type': 'application/json',
+      Authorization: '',
+      'x-api-key': 'test-api-key',
+    });
   });
 });
