49e241e82ab4130c5eb09753b00d38217f18b4fe
Santoshkumar Sharanappa Nateekar <nateekar@Santoshkumars-MacBook-Pro.local>
2025-05-27 11:21:34 -0700

    nala-initial-commit

---
+++ nala/features/compress-pdf/compress-pdf.page.cjs
@@ -0,0 +1,22 @@
+export default class CompressPdf {
+  constructor(page, nth = 0) {
+    this.page = page;
+    // compress-pdf widget locators
+    this.section = this.page.locator('.section').nth(nth);
+    this.compressPdf = this.page.locator('.compress-pdf.unity-enabled').nth(nth);
+    this.dropZone = this.compressPdf.locator('#drop-zone');
+    this.verbRow = this.compressPdf.locator('.verb-row').nth(0);
+    this.verbHeader = this.verbRow.locator('.verb-heading');
+    this.verbCopy = this.verbRow.locator('.verb-copy');
+    this.acrobatIcon = this.verbRow.locator('.acrobat-icon svg');
+    this.verbTitle = this.verbRow.locator('.verb-title');
+    this.verbImage = this.compressPdf.locator('.verb-image');
+    // file upload locators
+    this.uploadButton = this.page.locator('button.verb-cta', { hasText: 'Select a file' }).nth(nth);
+    this.fileInput = this.page.locator('input[type="file"]#file-upload');
+    // file upload error locators
+    this.verbError = this.page.locator('.verb-error');
+    this.verbErrorIcon = this.verbError.locator('.verb-errorIcon');
+    this.verbErrorText = this.verbError.locator('.verb-errorText');
+  }
+}
