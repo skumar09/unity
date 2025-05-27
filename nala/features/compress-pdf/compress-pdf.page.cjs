c7dff4d62a5c332634cb598b4b985512e46990d9
Santoshkumar Sharanappa Nateekar <nateekar@Santoshkumars-MacBook-Pro.local>
2025-05-27 15:22:50 -0700

    update the locator

--- nala/features/compress-pdf/compress-pdf.page.cjs
+++ nala/features/compress-pdf/compress-pdf.page.cjs
@@ -3,7 +3,7 @@ export default class CompressPdf {
     this.page = page;
     // compress-pdf widget locators
     this.section = this.page.locator('.section').nth(nth);
-    this.compressPdf = this.page.locator('.compress-pdf.unity-enabled').nth(nth);
+    this.compressPdf = this.page.locator('.compress-pdf').nth(nth);
     this.dropZone = this.compressPdf.locator('#drop-zone');
     this.verbRow = this.compressPdf.locator('.verb-row').nth(0);
     this.verbHeader = this.verbRow.locator('.verb-heading');
